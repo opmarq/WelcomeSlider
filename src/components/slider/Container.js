c04c7fa5e4833566679df29638249bdb717bdeba
OpmarQ <opmarq@gmail.com>
2019-01-17 18:20:51 +0100

    Create Slider Container

---
+++ src/components/slider/Container.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+import "../../grid.scss";
+
+class Container extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+
+  updateDimensions = () => {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  };
+
+  componentDidMount = () => {
+    window.addEventListener("resize", this.updateDimensions);
+  };
+
+  render() {
+    const { height, width } = this.state;
+    const { cols, rows } = this.props;
+
+    const cardHeight = Math.round(height / rows);
+    const cardWidth = Math.round(width / cols);
+
+    return (
+      <div className="slider__container">
+        {React.Children.map(this.props.children, child => {
+          return React.cloneElement(child, {
+            style: {
+              ...child.props.style,
+              border: "1px solid #555555",
+              height: cardHeight,
+              width: cardWidth
+            }
+          });
+        })}
+      </div>
+    );
+  }
+}
+
+Container.defaultProps = {
+  cols: 3,
+  rows: 2
+};
+
+export default Container;
